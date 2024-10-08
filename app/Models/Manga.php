34458950d07af61bea1026bf39dbad26a561305c
Chloé MARTINEZ <cmart@lmdsio.com>
2024-10-08 09:42:22 +0200

    Initial commit V1

---
+++ app/Models/Manga.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+class Manga extends Model
+{
+    protected $table='manga';
+    protected $primaryKey='id_manga';
+    public $timestamps=false;
+}
